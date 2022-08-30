9eae5e85e20497c50b82bbd4258c583f86765d19
Ayrat <gimranov45@gmail.com>
2022-08-30 08:56:49 -0700

    added random unsplash image search

--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,4 @@
 export const cardsContainer = document.getElementById('cards_container');
 export const listTitle = document.getElementById('list-title');
 export const inputForm = document.getElementById('form');
+export const spinnerGif = "../img/Loading_icon.gif";
